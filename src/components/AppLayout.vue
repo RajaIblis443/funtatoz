eba7a2e56239618e8ca37a6b38d1f0927c5c868f
NOOBPLAYER-DEBUG <77473102+NOOBPLAYER-DEBUG@users.noreply.github.com>
2025-05-30 18:46:46 +0700

    fix ui

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -24,12 +24,11 @@
       <div class="p-4 flex flex-col h-full">
         <!-- Logo -->
         <div class="flex items-center mb-8">
-          <div class="h-10 w-10 rounded-md bg-blue-600 flex items-center justify-center text-white mr-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-            </svg>
+          <div class="h-20 w-18 rounded-md bg-none flex items-center justify-center text-white mr-3">
+            <img src="../assets/logo final.png" alt="Logo">
+          </img>
           </div>
-          <h1 class="text-xl font-bold text-blue-600">FUNTATOZ</h1>
+          <h1 class="text-2xl font-bold text-black">FUNTATOZ</h1>
         </div>
 
         <!-- Navigation Menu -->
